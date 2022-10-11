e2fbfebeedbbbbb6263b66fcb698c875b8bc293d
shubham sharma <shubham.sharma6803@gmail.com>
2022-10-11 22:54:48 +0530

    changes

---
+++ FirstPage.js
@@ -0,0 +1,18 @@
+var element=function count(arr) {
+  return "Array has " + arr.length  + " Elements" ;
+
+}
+
+var add=function add(a,b) {
+  return `the sum of 2 elements ${a+b}`;
+
+}
+
+var pi=3.142
+
+module.exports=
+{
+  element:element,
+  add:add,
+  pi:pi
+}
---
+++ Second.js
@@ -0,0 +1,5 @@
+var RefrenceVariable= require('./FirstPage');
+
+console.log(RefrenceVariable.element(['ABC','BCD','EFG']));
+console.log(RefrenceVariable.add(5,8));
+console.log(RefrenceVariable.add(RefrenceVariable.pi,7));
