ccf60fe2e1d2866295636a026d49047aa08d2c33
shubham sharma <shubham.sharma6803@gmail.com>
2022-10-11 22:57:05 +0530

    changes

--- Second.js
+++ Second.js
@@ -2,4 +2,4 @@ var RefrenceVariable= require('./FirstPage');
 
 console.log(RefrenceVariable.element(['ABC','BCD','EFG']));
 console.log(RefrenceVariable.add(5,8));
-console.log(RefrenceVariable.add(RefrenceVariable.pi,7));
+console.log(RefrenceVariable.add(RefrenceVariable.pi,9));
